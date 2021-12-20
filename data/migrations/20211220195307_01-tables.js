852fe78560c797cc9f0482e7827bd271c0762423
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-20 11:56:28 -0800

    wip

---
+++ data/migrations/20211220195307_01-tables.js
@@ -0,0 +1,12 @@
+exports.up = async (knex) => {
+  await knex.schema.createTable("users", (users) => {
+    users.increments("user_id");
+    users.string("email", 200).notNullable().unique();
+    users.string("password", 200).notNullable();
+    users.timestamps(false, true);
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("users");
+};
