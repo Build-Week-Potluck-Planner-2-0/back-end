b1cbf1f5b218c102df585322f5f6d94774d25406
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-20 21:08:39 -0800

    knex cleaner set up, dev database is able to be migrated into

--- data/migrations/20211220195307_01-tables.js
+++ data/migrations/20211220195307_01-tables.js
@@ -2,6 +2,7 @@ exports.up = async (knex) => {
   await knex.schema.createTable("users", (users) => {
     users.increments("user_id");
     users.string("email", 200).notNullable().unique();
+    users.string("username", 200).notNullable().unique();
     users.string("password", 200).notNullable();
   });
 };
