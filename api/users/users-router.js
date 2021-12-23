de288ef658abe7ba5cc5f6c75553fafd1cfc373f
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-23 15:49:08 -0800

    wait

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,19 +1,20 @@
 const router = require("express").Router();
 const Users = require("./users-model");
+const restricted = require("../middleware/restricted");
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", restricted, (req, res, next) => {
   res.json(
     `delete user by id, delete all potlucks, delete all items, delete all invites related to ${req.params.id}`
   );
 });
 
-router.get("/", (req, res, next) => {
+router.get("/", restricted, (req, res, next) => {
   Users.getAll()
     .then((allUsers) => res.status(200).json(allUsers))
     .catch(next);
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", restricted, (req, res, next) => {
   res.json(
     `update information on ${req.params.id} user like username, not email or password`
   );
